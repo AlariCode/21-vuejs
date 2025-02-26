611c87a76dd0bb93a184360915c1a3ab904163ad
Alaricode <antonlarichev@gmail.com>
2025-02-26 15:06:43 +0300

    Шаблоны

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
-<script setup></script>
+<script setup>
+const a = 10;
+let b = "привет";
+let c = true;
+let d = {
+  a: 1,
+};
+let e = "<a href='/'>link</a>";
+</script>
 
 <template>
-  <header>лого</header>
-  <main>Привет!</main>
-  <footer>Футер</footer>
+  <header>{{ a }}</header>
+  <main>{{ b }}</main>
+  <div>{{ c }}</div>
+  <div v-html="e"></div>
+  <footer>{{ d.a }}</footer>
 </template>
 
 <style scoped></style>
