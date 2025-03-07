f60f760a360e279c980a9ad4dda3f06c6a345a0d
Alaricode <antonlarichev@gmail.com>
2025-03-07 17:40:01 +0300

    ref

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,29 @@
 <script setup>
+import { ref } from "vue";
 import CitySelect from "./components/CitySelect.vue";
 import Stat from "./components/Stat.vue";
-const data = {
+
+let savedCity = ref("Moscow");
+let data = ref({
   label: "Влажность",
   stat: "90%",
-};
-let savedCity = "Moscow";
+});
+let arr = ref([1]);
+let map = ref(new Map([["1", 1]]));
 
 function getCity(city) {
-  console.log(city);
-  savedCity = city;
+  savedCity.value = city;
+  data.value.stat = "20%";
+  arr.value.push(2);
+  map.value.set("2", 2);
 }
-
-const user = {
-  name: "Anton",
-};
-
-const handler = {
-  get(target, prop, receiver) {
-    console.log("Get value");
-    return target[prop];
-  },
-  set(obj, prop, value) {
-    if (prop == "name") {
-      console.log("Set value");
-      obj[prop] = value;
-      return true;
-    }
-  },
-};
-
-const proxy = new Proxy(user, handler);
-console.log(proxy.name);
-proxy.name = "sadfasdf";
-console.log(proxy.name);
 </script>
 
 <template>
   <main class="main">
     {{ savedCity }}
+    {{ arr }}
+    {{ map }}
     <Stat v-bind="data" />
     <Stat label="Осадки" stat="0%" />
     <CitySelect @select-city="getCity" />
